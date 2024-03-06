0611e2a98a7bbb7a0ce5a7725326da2bffe2a69c
Anastasia Shishmareva <stacy.shishmareva@gmail.com>
2024-03-07 01:22:52 +0300

    refactor: edit user module, create in-memory db, add users to db

---
+++ src/database/database.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@nestjs/common';
+import { User } from 'src/user/entities/user.entity';
+
+@Injectable()
+export class DatabaseService {
+  private users: User[] = [];
+  addUser(user: User) {
+    this.users.push(user);
+    return user;
+  }
+
+  getUsers() {
+    return this.users;
+  }
+
+  getUserById(id: string) {
+    return this.users.find((user) => user.id === id);
+  }
+
+  updateUser(user: User) {
+    const index = this.users.findIndex((u) => u.id === user.id);
+    this.users[index] = user;
+    return user;
+  }
+
+  deleteUser(id: string) {
+    const index = this.users.findIndex((u) => u.id === id);
+    this.users.splice(index, 1);
+    return id;
+  }
+}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,26 +1,31 @@
 import { Injectable } from '@nestjs/common';
+import { DatabaseService } from 'src/database/database.service';
+import { User } from './entities/user.entity';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 
 @Injectable()
 export class UserService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
-  }
+  constructor(private readonly databaseService: DatabaseService) {}
 
   findAll() {
-    return `This action returns all user`;
+    return this.databaseService.getUsers();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+  findOne(id: string) {
+    return this.databaseService.getUserById(id);
   }
-
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
+  create(createUserDto: CreateUserDto) {
+    return this.databaseService.addUser(
+      new User(createUserDto.login, createUserDto.password),
+    );
   }
-
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+  update(id: string, updateUserDto: UpdateUserDto) {
+    return this.databaseService.updateUser(
+      new User(updateUserDto.login, updateUserDto.password),
+    );
+  }
+  remove(id: string) {
+    return this.databaseService.deleteUser(id);
   }
 }
