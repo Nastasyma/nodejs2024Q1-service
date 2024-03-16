78a153079cb487e56b634795cb99a7bd95c90579
Anastasia Shishmareva <stacy.shishmareva@gmail.com>
2024-03-17 00:15:32 +0300

    refactor: delete in-memory db

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
-import { DatabaseModule } from './database/database.module';
 import { TrackModule } from './track/track.module';
 import { ConfigModule } from '@nestjs/config';
 import { ArtistModule } from './artist/artist.module';
@@ -14,7 +13,6 @@ import { PrismaModule } from './prisma/prisma.module';
   imports: [
     ConfigModule.forRoot(),
     UserModule,
-    DatabaseModule,
     TrackModule,
     ArtistModule,
     AlbumModule,
